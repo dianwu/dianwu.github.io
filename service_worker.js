767a5cc990622f908dd2b0d72870618f8a6abbd7
Dian <elgoogdian@gmail.com>
2016-04-24 23:42:54 +0800

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -8,7 +8,7 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('push', function(event) {
-    console.log('Received a push message', event);
+    console.log('Received a push message', event.data);
 
   var title = 'Yay a message.';  
   var body = 'We have received a push message.';  
